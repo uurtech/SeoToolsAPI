f277e6895f3b19729b853ad130d6a2fe4f5010c1
Ugur KAZDAL <ugur.kazdal@1ki3.com>
2015-12-24 11:20:25 +0200

    biçim etiketlerine kadar yazıldı
    yeni fonksiyonlar eklendi

--- class/html.class.php
+++ class/html.class.php
@@ -2,14 +2,6 @@
 
 /*
 
-html versiyonu
-karakter kodlaması
-meta başlık uzunluğu
-meta description
-meta keywords
-heading etiketleri
-
-toplam resim sayısı
 biçim etiketleri
 site içi bağlantı sayısı
 site dışına bağlantı sayısı
@@ -23,24 +15,23 @@ class HTML{
   function __construct($url){
       MetaTags($url);
   }
-  function htmlFunctionChecker($this->source){
+  functionFunctionChecker(){
     //<!DOCTYPE html> html5
     //<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
     return searchForAny($this->source,"<!DOCTYPE html>");
   }
 
-  function htmlCharsetChecker($this->source){
+  functionCharsetChecker(){
     return searchForAny($this->source,"charset");
   }
 
-  function htmlTitleChecker($this->source){
+  functionTitleChecker(){
     //70 chars max
     preg_match("/\<title\>(.*)\<\/title\>/i",$this->source,$title); // ignore case
     return strlen($title[1]);
   }
 
-
-  function htmlDescriptionChecker($this->source){
+  functionDescriptionChecker(){
     //160 chars max
     if(strlen($metaTags['description']) < 160){
       return true;
@@ -48,14 +39,42 @@ class HTML{
     return false;
   }
 
-  function htmlKeywordChecker($this->source){
+  functionKeywordChecker(){
     if(strlen($metaTags['keywords']) < 2){
       return true;
     }
     return false;
   }
+
+  function countHeadings(){
+    $count = 0;
+    $headingTags = array(
+      "<h1>","<h2>","<h3>","<h4>","<h5>","<h6>"
+    );
+    foreach($headingTags as $tag){
+      if(is_numeric(countForTags($tag))){
+        $count += countForTags($tag);
+      }
+    }
+    return $count;
+  }
+
+  function countImageTags(){
+    $count = explode("<img",$this->source);
+    return $count;
+  }
+
+  //search for spesific tags
+  function countForTags($this->source, $tag){
+    $doc = new DOMDocument();
+    $xml = $str->asXML();  // $str is your html string
+    $doc->loadXML($xml);
+    $bar_count = $doc->getElementsByTagName($tag)->length;
+    return $bar_count;
+  }
+
   //search for any word function
-  function searchForAny($this->source,$search)(
+  function searchForAny($search)(
   if(strpos($this->source,$search) !== FALSE){
     return true;
   }
